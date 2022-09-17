f0c1746e4ea9794143d74d0da3ea0f0bb339067b
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-09-17 15:23:04 +0530

    static variable and functions

--- oops.cpp
+++ oops.cpp
@@ -13,6 +13,13 @@ public:
 	// 	cout<<"simple constructor is called"<<endl;
 	// }
 
+	static int schoolId;
+
+	static int giveSchoolId()
+	{
+		return schoolId;
+	}
+
 	Student(int rollNo = 0)
 	{
 		this->rollNo = rollNo;
@@ -20,7 +27,8 @@ public:
 
 	Student(Student &S)
 	{
-		cout << "custom copy constructor/assignment-operator called (deep copy)" << endl;
+		cout << "custom copy constructor/assignment-operator called (deep copy)" << endl
+			 << endl;
 		this->rollNo = S.rollNo;
 		this->name = S.name;
 	}
@@ -46,23 +54,42 @@ public:
 		rollNo = (r) ? r : rollNo;
 		name = (n.size()) ? n : name;
 	}
+
+	~Student()
+	{
+		cout << "Destructor called" << endl
+			 << endl;
+	}
 };
+
+int Student::schoolId = 1911;
 
 int main()
 {
 	Student s1(10);
 	s1.setter("ramesh");
-	cout << "s1-> " << s1.nameGetter() << " " << s1.rollGetter() << endl;
+	cout << "s1-> " << s1.nameGetter() << " " << s1.rollGetter() << endl
+		 << endl;
 
 	Student *s2 = new Student;
 	s2->setter("prayas", 23);
-	cout << "s2-> " << s2->nameGetter() << " " << s2->rollGetter() << endl;
+	cout << "s2-> " << s2->nameGetter() << " " << s2->rollGetter() << endl
+		 << endl;
 
 	Student s3(*s2);
-	cout << "s3-> " << s3.nameGetter() << " " << s3.rollGetter() << endl;
+	cout << "s3-> " << s3.nameGetter() << " " << s3.rollGetter() << endl
+		 << endl;
+	delete s2;
 
 	Student s4 = s1;
-	cout << "s4-> " << s4.nameGetter() << " " << s4.rollGetter() << endl;
+	cout << "s4-> " << s4.nameGetter() << " " << s4.rollGetter() << endl
+		 << endl;
+
+	cout << "Calling static member variable " << Student::schoolId << endl
+		 << endl;
+
+	cout << "Calling static member function " << Student::giveSchoolId() << endl
+		 << endl;
 
 	return 0;
 }

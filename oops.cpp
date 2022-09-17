2cf8b854b69dc8bb4a3b611ea7dba5d51e20f9f3
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-09-17 11:28:43 +0530

    copy constructor

--- oops.cpp
+++ oops.cpp
@@ -9,29 +9,60 @@ private:
 	string name;
 
 public:
+	// Student(){
+	// 	cout<<"simple constructor is called"<<endl;
+	// }
+
+	Student(int rollNo = 0)
+	{
+		this->rollNo = rollNo;
+	}
+
+	Student(Student &S)
+	{
+		cout << "custom copy constructor/assignment-operator called (deep copy)" << endl;
+		this->rollNo = S.rollNo;
+		this->name = S.name;
+	}
+
+	Student(int rollNo, string name)
+	{
+		this->rollNo = rollNo;
+		this->name = name;
+	}
+
 	int rollGetter()
 	{
 		return rollNo;
 	}
+
 	string nameGetter()
 	{
 		return name;
 	}
-	void setter(int r, string n)
+
+	void setter(string n = NULL, int r = 0)
 	{
-		rollNo = r;
-		name = n;
+		rollNo = (r) ? r : rollNo;
+		name = (n.size()) ? n : name;
 	}
 };
 
 int main()
 {
-	Student s1;
-	s1.setter(22, "ramesh");
-	cout << s1.nameGetter() << " " << s1.rollGetter() << endl;
+	Student s1(10);
+	s1.setter("ramesh");
+	cout << "s1-> " << s1.nameGetter() << " " << s1.rollGetter() << endl;
 
 	Student *s2 = new Student;
-	s2->setter(23, "chomu");
-	cout << s2->nameGetter() << " " << s2->rollGetter() << endl;
+	s2->setter("prayas", 23);
+	cout << "s2-> " << s2->nameGetter() << " " << s2->rollGetter() << endl;
+
+	Student s3(*s2);
+	cout << "s3-> " << s3.nameGetter() << " " << s3.rollGetter() << endl;
+
+	Student s4 = s1;
+	cout << "s4-> " << s4.nameGetter() << " " << s4.rollGetter() << endl;
+
 	return 0;
 }
